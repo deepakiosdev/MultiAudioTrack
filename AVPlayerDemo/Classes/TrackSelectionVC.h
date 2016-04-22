ece6c08afe15ebf2609fd33e24be2ceaf1bb08da
dipak <dipak@DEV-024s-iMac.local>
2016-04-22 18:25:26 +0530

    First commit.

---
+++ AVPlayerDemo/Classes/TrackSelectionVC.h
@@ -0,0 +1,13 @@
+//
+//  TrackSelectionVC.h
+//  AVPlayerDemo
+//
+//  Created by dipak on 4/22/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrackSelectionVC : UITableViewController
+@property (nonatomic, strong) NSArray* tracksArray;
+@end
